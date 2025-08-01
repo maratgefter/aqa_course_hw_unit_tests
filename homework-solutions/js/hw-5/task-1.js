de2b24bc43d67debc0c1979a7aea4cef0b50cddd
Marat <margefter@gmail.com>
2025-08-01 15:02:16 +0300

    done task-1

--- homework-solutions/js/hw-5/task-1.js
+++ homework-solutions/js/hw-5/task-1.js
@@ -5,6 +5,15 @@
  */
 
 let evenNumbersResult = '';
+let a = 10;
+while (a <= 10 && a >= 0) {
+    if (a % 2 === 0 && a !== 0) {
+        evenNumbersResult += `${a}-`
+    } else if (a % 2 === 0 && a === 0) {
+        evenNumbersResult += `${a}`
+    }
+    a--;
+}
 
 /**
  * Создать строку из 5 строк с увеличивающимся количеством смайликов ":)".
@@ -18,6 +27,14 @@ let evenNumbersResult = '';
  */
 
 let smilePatternResult = '';
+let smile = ":)";
+for (let i = 1; i <= 5; i++) {
+  if (i === 5){
+smilePatternResult += `${smile.repeat(i)}`;
+  } else {
+smilePatternResult += `${smile.repeat(i)}\n`;
+  }
+}
 
 /**
  * Заменить все пробелы в переменной text на "1".
@@ -25,6 +42,6 @@ let smilePatternResult = '';
  * Ожидаемый результат: 'Hello!1I1am1a1JS1student!'
  */
 const text = 'Hello! I am a JS student!';
-let replaceSpacesWithOneResult = '';
+let replaceSpacesWithOneResult = text.replaceAll(" ", 1);
 
 export { evenNumbersResult, smilePatternResult, replaceSpacesWithOneResult };

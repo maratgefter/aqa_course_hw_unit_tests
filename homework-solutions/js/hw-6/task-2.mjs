52ada4d96d6af01808e174f1d261b0ced687663a
Marat <margefter@gmail.com>
2025-08-06 02:03:48 +0300

    done task-2

--- homework-solutions/js/hw-6/task-2.mjs
+++ homework-solutions/js/hw-6/task-2.mjs
@@ -13,7 +13,41 @@
   const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 */
 
-let resultUnique;
+let resultUnique = [];
 let resultNull;
+const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
+const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+let result1 = [];
+let result2 = [];
+
+for (let myPizza of myPizzasT1) {
+  let matchFound = false;
+  for (let competitorPizza of competitorPizzas) {
+    if (myPizza.toLowerCase() === competitorPizza.toLowerCase()) {
+      matchFound = true;
+      break;
+    }
+  }
+  if (!matchFound) {
+    result1.push(myPizza);
+  }
+}
+result1.length ? resultUnique = result1 : resultNull = null;
+
+for (let myPizza of myPizzasT2) {
+  let matchFound = false;
+  for (let competitorPizza of competitorPizzas) {
+    if (myPizza.toLowerCase() === competitorPizza.toLowerCase()) {
+      matchFound = true;
+      break;
+    }
+  }
+  if (!matchFound) {
+    result2.push(myPizza);
+  }
+}
+result2.length ? resultUnique = result2 : resultNull = null;
+
 
 export { resultNull, resultUnique };

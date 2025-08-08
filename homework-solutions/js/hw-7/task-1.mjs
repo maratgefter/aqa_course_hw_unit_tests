5ac5400a13c03e354ca84f99969de3e4172847fb
Marat <margefter@gmail.com>
2025-08-08 14:09:37 +0300

    done task-1

--- homework-solutions/js/hw-7/task-1.mjs
+++ homework-solutions/js/hw-7/task-1.mjs
@@ -5,9 +5,10 @@
   - Например: mergeArrays([1,2], [3,4], [5,6]) // [1,2,3,4,5,6]
   - Решить с использованием Spread operator
 */
-function mergeArrays() {
-  // Ваш код
+function mergeArrays(...arrays) {
+  return [...[].concat(...arrays)];
 }
+
 /*
   2. Devide by _
     - Написать функцию, которая преобразует любое предложение в вот_Такой_Вот_Вид и возвращает его. 
@@ -15,7 +16,20 @@ function mergeArrays() {
     - Пример: I am super engineer => i_Am_Super_Engineer
   */
 function devideBy(sentence) {
-  // Ваш код
+  if(typeof sentence !== 'string'){
+    return 'Invalid data type';
+  } else {
+    sentence = sentence.trim().split(/\s+/);
+    for(let i = 0; i < sentence.length; i++){
+      let word = sentence[i];
+      if(i === 0){
+        sentence[i] = word.charAt(0).toLowerCase() + word.slice(1).toLowerCase();
+      } else {
+        sentence[i] = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+      }
+    }
+    return sentence.join('_');
+  }
 }
 /*
   3. Фибаначчи
@@ -26,7 +40,9 @@ function devideBy(sentence) {
     - Например fibonacci(8) //21
   */
 function fibonacci(n) {
-  // Ваш код
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  return (fibonacci(n - 1) + fibonacci(n - 2));
 }
 
-export { mergeArrays, fibonacci, devideBy };
+// export { mergeArrays, fibonacci, devideBy };

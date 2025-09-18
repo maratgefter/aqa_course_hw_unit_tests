91312e881b41e0d32665b495a3123cbd3fa9c78d
Marat <margefter@gmail.com>
2025-09-18 12:59:38 +0300

    done task-3

--- homework-solutions/js/hw-15/task-2.ts
+++ homework-solutions/js/hw-15/task-2.ts
@@ -3,3 +3,23 @@
 Если значение не найдено, функция должна возвращать undefined.
 Используйте keyof для типизации ключей объекта
 */
+
+function getKeyByValue<T extends Record<string, any>, V>(
+  obj: T,
+  value: V
+): keyof T | undefined {
+  for (const key in obj) {
+    if (obj[key] === value) {
+      return key as keyof T;
+    }
+  }
+  return undefined;
+}
+
+const employee = {
+    'name': 'Petya',
+    'age': 21,
+    'skill': 'QA'
+}
+console.log(getKeyByValue(employee, 21));
+console.log(getKeyByValue(employee, 'test'));
